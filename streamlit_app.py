32f81aa52377613ec3d32b1868b546a1578746a4
hozl03 <hozl-wp21@student.tarc.edu.my>
2024-09-14 17:28:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 st.title('House Price Prediction')
 
 st.write('This is an app builds a house price prediction machine learning model')
+
+df = pd.read_csv('')
