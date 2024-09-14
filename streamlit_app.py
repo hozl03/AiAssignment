8d5d6dc0e106a379e7a745e2f1b8edd98fb9ef61
hozl03 <hozl-wp21@student.tarc.edu.my>
2024-09-14 21:30:13 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,8 +56,7 @@ with st.sidebar:
                                                   'Duplex', 'Townhouse End Unit', 'Townhouse Inside Unit'))
     
     overallQuality = st.slider("Rates the overall material and finish of the house", 1, 10, 5)
-rating(0)
-    # st.write("The overall material and finish of the house is ", rating(overallQuality + 1))
+    st.write("The overall material and finish of the house is ", rating(overallQuality - 1))
 
 
 
