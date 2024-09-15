d7c13a3fad1512db2e8c4bcd577e20c2ebbb6cce
hozl03 <hozl-wp21@student.tarc.edu.my>
2024-09-15 16:09:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,21 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import datetime
-from sklearn.preprocessing import StandardScaler
+
 from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+
+import sklearn # import the module so you can use it
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
+from sklearn.ensemble import RandomForestRegressor
+
+from sklearn.svm import SVR
+from xgboost import XGBRegressor
+from sklearn.preprocessing import PolynomialFeatures
 
 import joblib
 
